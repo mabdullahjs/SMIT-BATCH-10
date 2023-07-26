0bdb613b3c5af575c8cea21a8765ef8e12183dad
Muhammad Abdullah <mabdullah2037@outlook.com>
2023-07-26 13:01:44 +0500

    javascript hello world

---
+++ Javascript/class-19/app.js
@@ -0,0 +1,24 @@
+// var names = "abdullah"
+// var age = 10
+// console.log(names)
+// console.log(age)
+
+
+// var num1 = 10;
+// var num2 = 20;
+// console.log(num1 + num2);
+
+var names = "Muhammad Abdullah";
+var age = 19;
+// console.log("My name is "  + names + " and my age is " + age + ".");
+// document.write("My name is "  + names + " and my age is " + age + ".");
+// alert("My name is "  + names + " and my age is " + age + ".");
+var isOk = true;
+console.log(isOk)
+
+//String
+//Number
+//boolean
+
+//undefined
+//object
