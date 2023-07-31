3d47244fa1878314e8adcf2311ca7db0789fc87e
Muhammad Abdullah <mabdullah2037@outlook.com>
2023-07-31 12:54:50 +0500

    operators completed

---
+++ Javascript/class-20/script.js
@@ -0,0 +1,140 @@
+// var names = "abdullah"
+// var age = 10
+// console.log(names)
+// console.log(age)
+
+
+// var num1 = 10;
+// var num2 = 20;
+// console.log(num1 + num2);
+
+// var names = "Muhammad Abdullah@1233";
+// var age = 19;
+// console.log("My name is "  + names + " and my age is " + age + ".");
+// document.write("My name is "  + names + " and my age is " + age + ".");
+// alert("My name is "  + names + " and my age is " + age + ".");
+// var isOk = true;
+// console.log(isOk)
+
+//String
+//Number
+//boolean
+
+//undefined
+//object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Arithmatic Operator
+
+// +
+// -
+// *
+// /
+// %
+
+//Assignment Operator
+// =
+// +=
+// -=
+// /=
+// *=
+
+//Comparision Operator
+
+// >
+// <
+// ==
+// ===
+// !=
+// >=
+// <=
+
+//logical Operator 
+
+
+// var num1 = 10;
+// var num2 = 20;
+// var sum = num1 - num2;
+// console.log(sum);
+
+// var num1 = 20;
+// console.log(num1)
+// num1 = 40;
+// console.log(num1)
+
+// var num1 = 20;
+// // num1 = num1 + 30;
+// num1 *= 30;
+// console.log(num1);
+
+
+// var num1 = "10"
+// var num2 = 10
+// console.log(typeof("num1 > num2"));
+// console.log(num2 === num1);
+// console.log(num2 != num1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var names = Number(prompt("enter your name"));
+// console.log(typeof(names));
+// console.log(names);
