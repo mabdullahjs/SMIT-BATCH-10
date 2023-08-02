0e70b160ade92a4e4f9759c66fdda68c4150ddb1
Muhammad Abdullah <mabdullah2037@outlook.com>
2023-08-02 13:01:50 +0500

    if else completed

---
+++ Javascript/class-21/script.js
@@ -0,0 +1,43 @@
+
+
+
+// var num = 1;
+// var result = num++
+
+// var names = prompt("Enter your name");
+// var chemMarks = +prompt("Enter chemistry marks");
+// var physMarks = +prompt("Enter physics marks");
+// var mathMarks = +prompt("Enter Maths marks");
+// var sindhiMarks = +prompt("Enter Sindhi marks");
+// var totalMarks = 400;
+// var studentMarks = chemMarks + physMarks + mathMarks + sindhiMarks;
+// var percentage = studentMarks / totalMarks * 100;
+// // console.log(names , chemMarks , physMarks , mathMarks , sindhiMarks)
+// console.log(names + " total marks is " + studentMarks + " out of " + totalMarks + " and percentage is " + percentage + " %");
+
+
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// console.log(resultOfComputation);
+
+// var num = 10;
+
+// if (num > 5 && num < 5 || num > 15) {
+//     console.log("if")
+// } else {
+//     console.log("else");
+// }
+
+
+var percentage = 30;
+if (percentage >= 80) {
+    console.log("your grade is A+")
+} else if (percentage >= 70) {
+    console.log("your grade is A")
+} else if (percentage >= 60) {
+    console.log("your grade is B")
+} else if (percentage >= 50) {
+    console.log("your grade is C")
+}
+else {
+    console.log("Bhai tum hogaye ho fail.");
+}
