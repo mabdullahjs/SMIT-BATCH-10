495fe3bfc010948740fa90d9ba51f7c0683e3cb0
Muhammad Abdullah <mabdullah2037@outlook.com>
2023-08-07 13:07:51 +0500

    switch statement completed

---
+++ Javascript/05-Switch-statement/app.js
@@ -0,0 +1,83 @@
+//var 
+//let
+//const
+
+// let name1 = "Muhammad Abdullah";
+// const name2 = "Muhammad Abdullahs";
+
+// name1 = "Usman";
+// console.log(name1)
+
+// name2 = "ibrahim";
+// console.log(name2)
+
+// let num1 = 10;
+// switch (true) {
+//     case (num1 < 5):
+//         console.log("pehla case chal gaya");
+//         // break lagana laazmi ha
+//     case (num1 > 6):
+//         console.log("dosra case");
+//         break
+//     default:
+//         console.log("default chal gaya");
+// }
+
+// let number = 9;
+
+// if (number % 3 == 0) {
+//     console.log("number is divsible by 3")
+// }
+// else {
+//     console.log("Number is not divisible by 3")
+// }
+
+// let number = 2;
+
+// if (number % 2 == 0) {
+//     console.log("number is even")
+// }
+// else {
+//     console.log("Number is odd")
+// }
+
+// let age = 21;
+
+// if (age >= 18) {
+//     console.log("Old enough")
+// }
+// else {
+//     console.log("too young")
+// }
+
+// let myName = "Abdullah";
+// let names = prompt("enter your name");
+// if (myName === names) {
+//     console.log("Hello");
+// }
+
+
+
+let num1 = +prompt("Enter num 1");
+let num2 = +prompt("Enter num 2");
+let operator = prompt("Enter operator");
+
+// if(operator === "+"){
+//     let sum = num1 + num2;
+//     console.log(sum);
+// }else if(operator === "-"){
+//     let sub = num1 - num2;
+//     console.log(sub);
+// }else if(operator === "*"){
+//     let multiply = num1 * num2;
+//     console.log(multiply);
+// }else if(operator === "/"){
+//     let division = num1 / num2;
+//     console.log(division);
+// }else if(operator === "%"){
+//     let modulus = num1 % num2;
+//     console.log(modulus);
+// }
+// else{
+//     console.log("bhaijan operator daal sahi");
+// }
