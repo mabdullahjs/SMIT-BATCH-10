e98490ff7e1188dae96107418a25c2c3a80ff19d
Muhammad Abdullah <mabdullah2037@outlook.com>
2023-08-09 12:49:47 +0500

    functions basic

---
+++ Javascript/06-Functions/app.js
@@ -0,0 +1,40 @@
+// let a = 10;
+// let b = "scsadsad";
+// // let sumOfAB = a + b;
+// // console.log(typeof(b));
+// sumOfTwoNumber(a , b)
+
+
+// let c = 110;
+// let d = 210;
+// // let sumOfCD = c + d;
+// // console.log(sumOfCD);
+// sumOfTwoNumber(c , d)
+
+
+// let e = 1100;
+// let f = 2100;
+// // let sumOfEF = e + f;
+// // console.log(sumOfEF);
+// sumOfTwoNumber(e , f)
+
+
+// function sumOfTwoNumber (i , h){
+//     if(typeof(i) == "number" && typeof(h) == "number"){
+//         let sum = i + h;
+//         console.log(sum);
+//     }
+//     else{
+//         console.log("bhaijan number bhej");
+//     }
+// }
+
+// sumOfTwoNumber(50 , 40);
+
+
+
+
+function ramoKaka(dish1, dish2, dish3, name) {
+    console.log("bhaijan appka order jisma " + dish1 + " " + dish2 + " " + dish3 + " " + "haa or appka naam " + name + " ha");
+}
+ramoKaka("karela", "bhindi", "roti", "abdullah");
