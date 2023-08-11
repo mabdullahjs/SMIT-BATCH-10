5a098a6b9fa3bb3587a08d7c579849b188cd1029
Muhammad Abdullah <mabdullah2037@outlook.com>
2023-08-11 12:49:59 +0500

    return keyword and DOM intro

--- Javascript/06-Functions/app.js
+++ Javascript/06-Functions/app.js
@@ -34,7 +34,96 @@
 
 
 
-function ramoKaka(dish1, dish2, dish3, name) {
-    console.log("bhaijan appka order jisma " + dish1 + " " + dish2 + " " + dish3 + " " + "haa or appka naam " + name + " ha");
-}
-ramoKaka("karela", "bhindi", "roti", "abdullah");
+// function ramoKaka(dish1, dish2, dish3, name) {
+//     console.log("bhaijan appka order jisma " + dish1 + " " + dish2 + " " + dish3 + " " + "haa or appka naam " + name + " ha");
+// }
+// ramoKaka("karela", "bhindi", "roti", "abdullah");
+
+
+
+
+
+
+
+
+
+
+
+// function istekhara(name, governmentJob, salery, car, house, bankBalance) {
+//     if (governmentJob === true && salery >= 90000 && car === "civic" && house === true && bankBalance >= 900000) {
+//         return ("istekhara ma haan haa " + name + " larka bhat acha ha.")
+//     }
+//     else {
+//         return ("samjh ma nahi aya koi or dekho.");
+//     }
+// }
+
+// const result = istekhara("Asad", true, 100000, "civic", true, 1100000);
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fname = "ajmal";
+// console.log("abdullah");
+
+// function sumOfTwoNumber(i, h) {
+//     let sum = i + h;
+//     // console.log(sum);
+//     return sum
+// }
+
+// const total = sumOfTwoNumber(10 , 20);
+// console.log(total);
+
+
+
+
+
+// function sum (num1 , num2){
+//     return num1 + num2;
+// }
+
+// console.log(sum(10 , 20));
+
+
+// function sum (num1 , num2){
+//     const total =  num1 + num2;
+// }
+
+// console.log(sum(10 , 20));
+
+
+
+
+// let a = 10;
+// a = "abdullah";
+// console.log(a);
+
+// const b = 10;
+// b = 10;
+
+
+
+
+
+
+// const heading01 = document.querySelector(".first-head");
+// heading01.innerHTML = "Hello <br/> dom";
+// console.log(heading01);
+
+
+const list = document.querySelector("#list");
+// list.innerHTML = "<li>Abdullah</li>"
+console.log(list.innerHTML);
