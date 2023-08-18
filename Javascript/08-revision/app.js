1f3b70b08b5f19a5d3c4783bb4ffbc94684eca07
Muhammad Abdullah <101110601+mabdullahjs@users.noreply.github.com>
2023-08-18 12:49:11 +0500

    js revision

---
+++ Javascript/08-revision/app.js
@@ -0,0 +1,93 @@
+
+// var num = 10;
+// num += 20;
+
+// > 
+// <
+// ==
+// ===
+
+// 10 >= 5 || 10 <= 5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function cal() {
+//     const num1 = +(document.querySelector("#number1").value);
+//     const num2 = +(document.querySelector("#number2").value);
+//     const operator = document.querySelector("#operator").value;
+//     const calculate_result = document.querySelector("#result");
+//     // console.log(num1);
+//     // console.log(num2);
+//     // console.log(operator);
+
+//     switch (operator) {
+//         case "+":
+//             calculate_result.innerHTML = num1 + num2;
+//             calculate_result
+//             break;
+//         case "-":
+//             calculate_result.innerHTML = num1 - num2;
+//             break;
+//         case "*":
+//             calculate_result.innerHTML = num1 * num2
+//             break;
+//         case "/":
+//             calculate_result.innerHTML = num1 / num2;
+//             break;
+
+//         default:
+//             calculate_result.innerHTML = "Error";
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function change_color() {
+//     const para = document.querySelector("#paragraph");
+//     if (para.style.backgroundColor == "black") {
+//         para.style.backgroundColor = "white";
+//         para.style.color = "black";
+//     }
+//     else {
+//         para.style.backgroundColor = "black";
+//         para.style.color = "white";
+//     }
+// }
+
+
+
+
+
