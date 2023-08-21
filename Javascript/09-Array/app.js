fb2ebb83ca8b4053c98c72c5dc541bc3a6a43719
Muhammad Abdullah <101110601+mabdullahjs@users.noreply.github.com>
2023-08-21 12:49:23 +0500

    arrays and its methods

---
+++ Javascript/09-Array/app.js
@@ -0,0 +1,236 @@
+// let num = [1, 2, 3, 4, 5];
+// console.log(num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//join
+//concat
+//sort
+//reverse
+//indexof
+
+
+// let fruits = ['grapes', 'apple', 'watermelon', 'mango', 'peach', 'nashpati'];
+// // fruits.push('peach');
+// // fruits.pop();
+// // fruits.unshift('nashpati');
+// // fruits.shift();
+// let eaten = fruits.slice(1 , 3);
+// // fruits.splice(1, 3 , 'kiwi' , 'banana');
+
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let fruits = ['grapes', 'apple', 'watermelon', 'mango', 'peach', 'nashpati'];
+// let eaten = fruits.slice()
+// console.log(eaten);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let bikes = ["CD-70", "CG-125", "S-150", "S-110"]
+// bikes.splice(1, 1, "suzuki");
+// console.log(bikes);
+
+
+
+// let names = 'my name is Muhammad Abdullah khan and my age is 20';
+// let newArr = names.split(' ');
+// console.log(newArr)
+
+
+
+
+
+// let names = 'my name is Muhammad Abdullah khan and my age is 20';
+// let newArr = names.split(' ');
+// newArr[4] = "usman";
+// newArr.splice(6);
+// console.log(newArr);
+// let newStr = newArr.join(' ');
+// console.log(newStr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let array1 = [1, 2, 3];
+// let array2 = [4, 5, 6];
+// let newArr = array1.concat(array2);
+// console.log(newArr);
+// newArr.reverse();
+// console.log(newArr);
+
+
+// let arr = [9, 6, 3, 7, 2];
+// arr.sort();
+// console.log(arr);
+// const indexNumber = arr.indexOf(36);
+// console.log(indexNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
