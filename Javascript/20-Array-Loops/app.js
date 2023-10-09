ea8f027d46c326144f75f80ff64f3b4ad6deaf31
Muhammad Abdullah <101110601+mabdullahjs@users.noreply.github.com>
2023-10-09 12:56:09 +0500

    map

--- Javascript/20-Array-Loops/app.js
+++ Javascript/20-Array-Loops/app.js
@@ -99,7 +99,7 @@
 
 
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 //for each
 
@@ -179,44 +179,44 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
 
-const books = [
-    {
-        title: "Book 1",
-        author: "Author 1",
-        publishedYear: 2021,
-        subject: "Science"
-    },
-    {
-        title: "Book 2",
-        author: "Author 2",
-        publishedYear: 2019,
-        subject: "History"
-    },
-    {
-        title: "Book 3",
-        author: "Author 3",
-        publishedYear: 2020,
-        subject: "Fiction"
-    },
-    {
-        title: "Book 4",
-        author: "Author 4",
-        publishedYear: 2025,
-        subject: "Fiction"
-    },
-    {
-        title: "Book 5",
-        author: "Author 5",
-        publishedYear: 2018,
-        subject: "chemistry"
-    },
-    {
-        title: "Book 6",
-        author: "Author 6",
-        publishedYear: 2022,
-        subject: "Fiction"
-    },
-];
+// const books = [
+//     {
+//         title: "Book 1",
+//         author: "Author 1",
+//         publishedYear: 2021,
+//         subject: "Science"
+//     },
+//     {
+//         title: "Book 2",
+//         author: "Author 2",
+//         publishedYear: 2019,
+//         subject: "History"
+//     },
+//     {
+//         title: "Book 3",
+//         author: "Author 3",
+//         publishedYear: 2020,
+//         subject: "Fiction"
+//     },
+//     {
+//         title: "Book 4",
+//         author: "Author 4",
+//         publishedYear: 2025,
+//         subject: "Fiction"
+//     },
+//     {
+//         title: "Book 5",
+//         author: "Author 5",
+//         publishedYear: 2018,
+//         subject: "chemistry"
+//     },
+//     {
+//         title: "Book 6",
+//         author: "Author 6",
+//         publishedYear: 2022,
+//         subject: "Fiction"
+//     },
+// ];
 
 
 // const filteredBooks = books.filter(item => item.publishedYear > 2020 && item.subject === 'Fiction');
@@ -229,6 +229,22 @@ const books = [
 
 
 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const mappedArr = arr.map(item => item + 10).filter( item => item > 15).map(item => item * 2);
+
+// console.log(mappedArr);
+
+
+// const mappedArr = arr.map( item => item + 10).filter( item => item % 2 === 0).filter( item => item > 15);
+// console.log(mappedArr);
+
+
+
+
+//reduce
+//set timeout
+//set interval
 
 
 
@@ -243,16 +259,26 @@ const books = [
 
 
 
-let filteredBooks = books.filter(function (item) {
-    return item.subject === 'Fiction' 
-})
 
-console.log(filteredBooks);
 
-const div = document.querySelector('div');
-filteredBooks.forEach(function (item){
-    div.innerHTML += `<h1>${item.title}</h1>`
-})
+
+
+
+
+
+
+
+
+// let filteredBooks = books.filter(function (item) {
+//     return item.subject === 'Fiction'
+// })
+
+// console.log(filteredBooks);
+
+// const div = document.querySelector('div');
+// filteredBooks.forEach(function (item){
+//     div.innerHTML += `<h1>${item.title}</h1>`
+// })
 
 
 
