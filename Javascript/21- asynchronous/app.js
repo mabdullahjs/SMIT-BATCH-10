d9aaeccf8dd5b4456537aaee4525668159a874c3
Muhammad Abdullah <101110601+mabdullahjs@users.noreply.github.com>
2023-10-13 12:50:19 +0500

    async and sync programming

---
+++ Javascript/21- asynchronous/app.js
@@ -0,0 +1,124 @@
+//sync || async
+
+
+// console.log('hello world');
+// console.log('hello world 02');
+// console.log('hello world 03');
+
+// const data = request to the server
+// console.log(data);
+
+
+
+// const task1 = 5second
+// const task2 = 2second
+// const task3 = 7second
+// const task4 = 1second
+
+
+
+// setTimeout
+// setInterval
+// axios
+// promises
+// async await
+
+
+
+
+// console.log('hello world');
+
+
+
+
+
+// setTimeout(()=>{
+//     console.log('line 4');
+// } , 0)
+// console.log('line 1');
+// console.log('line 2');
+// console.log('line 3');
+// console.log('line 5');
+
+
+
+
+
+// try{
+//     aler('hello world')
+// }catch(error){
+//     console.log(error);
+// }
+
+
+
+// // aler('hello world');
+// console.log('hello world');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
