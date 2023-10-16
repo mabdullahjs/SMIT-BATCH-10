448124fb97554c15dacf8c716c25f784aeb17e45
Muhammad Abdullah <101110601+mabdullahjs@users.noreply.github.com>
2023-10-16 12:49:06 +0500

    weather app assignment

--- Javascript/21- asynchronous/app.js
+++ Javascript/21- asynchronous/app.js
@@ -69,6 +69,170 @@
 
 
 
+//setTimeOut
+//set interval
+//promises
+//async and await
+//axios
+
+
+
+
+
+// const shaadiScnz = new Promise((resolve , reject)=>{
+//     const bankBalance = 4000000;
+//     if(bankBalance > 2000000){
+//         resolve('shaadi mubarak')
+//     }else{
+//         reject('meri rooh tumhara pass rhaigi..')
+//     }
+// })
+
+
+// shaadiScnz.then((res)=>{
+//     console.log(res);
+//     console.log('then chal rha ha');
+// }).catch((err)=>{
+//     console.log(err);
+//     console.log('catch chal rha ha');
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function shaadiScnz(bankBalance) {
+//     return new Promise((resolve, reject) => {
+//         if (bankBalance > 1000000) {
+//             resolve('Kal rasam krlainga..')
+//         } else {
+//             reject('rooh tumhara pass rhaigi.. AJ ka baad message mat krna.')
+//         }
+//     })
+// }
+
+
+// shaadiScnz(5000000)
+// .then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// })
+
+
+
+
+
+// axios('https://www.server.com').then((res)=>{
+//     console.log(res.data);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+
+
+
+
+
+
+// async function shaadiScnz() {
+//     try {
+//         const res = await axios.get('https://jsonplaceholder.typicode.com/usesrs');
+//         console.log(res.data);
+//     } catch (error) {
+//         console.log(error);
+//         alert('data not found');
+//     }
+// }
+
+// shaadiScnz()
+
+
+
+
+
+
+
+
+
+
+// const div = document.querySelector('.container');
+// axios.get('https://jsonplaceholder.typicode.com/users')
+// .then((res)=>{
+//     console.log(res.data);
+//     res.data.map((item)=>{
+//         div.innerHTML += `
+//         <h1>Name: ${item.name}</h1>
+//         <h2>Id: ${item.id}</h2>
+//         <h2>city: ${item.address.city}</h2>
+//         <hr/>
+        
+//         `
+//     })
+
+// }).catch((err)=>{
+//     console.log(err);
+//     alert('data not found')
+// })
+
+
+console.log(navigator.geolocation);
+
+
+
+const div = document.querySelector('.container');
+axios.get(`http://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=${'sasasasas'}`)
+.then((res)=>{
+    console.log(res.data);
+    // res.data.map((item)=>{
+    //     div.innerHTML += `
+    //     <h1>Name: ${item.name}</h1>
+    //     <h2>Id: ${item.id}</h2>
+    //     <h2>city: ${item.address.city}</h2>
+    //     <hr/>
+        
+    //     `
+    // })
+
+}).catch((err)=>{
+    console.log(err);
+    alert('data not found')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
