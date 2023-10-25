4ee14404bdeb19a54c3c73c0e67707663721530c
Muhammad Abdullah <101110601+mabdullahjs@users.noreply.github.com>
2023-10-25 13:00:58 +0500

    es6 remaining topics

---
+++ Javascript/24- ES6/app.js
@@ -0,0 +1,129 @@
+
+
+// var
+// redeclare true
+// reassign true
+
+// let
+// redeclare false
+// reassign true
+
+// const
+// redeclare false
+// reassign false
+
+
+// var name1 = 'abdullah'; 
+// var name1 = 'abd';
+// name1 = 'a';
+// console.log(name1);
+
+// let name2 = 'abdullah' ;
+// name2 = 'abdullah';
+// console.log(name2);
+
+// const name3 = 'abdullah3'
+// console.log(name3);
+
+
+
+
+// function greetUser(){
+//     if(true){
+//         const names = 'abdullah';
+//         console.log(`hello ${names}`);
+//     }
+//     console.log(names);
+// }
+
+// greetUser()
+
+
+
+
+
+
+
+
+
+
+// template literals
+
+// var names = 'abdullah'
+// console.log('my name is ' + names);
+// console.log(`my name is ${names}`);
+
+
+
+//ternary operators
+
+// var bankBalance = 20000;
+// if(bankBalance > 400000){
+//     console.log('shaadi mubarak');
+// }else{
+//     console.log('istekhara ma naa aya ha ammi zehr kha laingi.');
+// }
+
+// bankBalance > 400000 ? console.log('shaadi mubarak') : console.log('ammi na zehr khalia');
+
+
+
+
+
+// bankBalance > 400000 ? console.log('shaadi mubarak') : bankBalance > 200000 ? console.log('abba nahi maan rha.'): console.log('larka acha ha');
+
+
+
+// spread operators
+
+// const arr = [1, 2, 4, 5, 6];
+// const arr2 = [...arr, 7, 8, 9];
+// console.log(arr2);
+
+
+function sumVal(...num) {
+    console.log(num);
+}
+sumVal(1,2,3,4,5,6,7,8,9);
+
+
+
+
+// object destructuring
+// lexical scoping
+//modulus
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
