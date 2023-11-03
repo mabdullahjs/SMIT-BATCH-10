1339163a53e8da5e9f11ecd5fffb43155894aa72
Muhammad Abdullah <101110601+mabdullahjs@users.noreply.github.com>
2023-11-03 12:55:30 +0500

    firestore database

--- 03-Firebase/02-Social-App/config.js
+++ 03-Firebase/02-Social-App/config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBkU7hUSCIU-_L7L2XhNt1b_XUqPx9660U",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
--- 03-Firebase/02-Social-App/home.js
+++ 03-Firebase/02-Social-App/home.js
@@ -1,5 +1,9 @@
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-import { auth } from "./config.js";
+import { auth, db } from "./config.js";
+import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+
+
+//user login or logout function
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
@@ -11,14 +15,60 @@ onAuthStateChanged(auth, (user) => {
 });
 
 
+//logout function
+
 const logout = document.querySelector('#logout-btn');
 
-logout.addEventListener('click' , ()=>{
+logout.addEventListener('click', () => {
     signOut(auth).then(() => {
         console.log('logout successfully');
         window.location = 'index.html'
-      }).catch((error) => {
+    }).catch((error) => {
         console.log(error);
-      });
-      
+    });
+})
+
+
+
+
+const form = document.querySelector('#form');
+const title = document.querySelector('#title');
+const description = document.querySelector('#description');
+const card = document.querySelector('#card');
+
+const arr = []
+async function getDataFromFirestore() {
+    const querySnapshot = await getDocs(collection(db, "posts"));
+    querySnapshot.forEach((doc) => {
+        arr.push(doc.data());
+    });
+    console.log(arr);
+    arr.map((item) => {
+        card.innerHTML += `
+        <div class="card mt-2">
+        <div class="card-body">
+            <p><span class="h4">Title:</span>${item.title}</p>
+            <p><span class="h4">Description:</span>${item.description}</p>
+        </div>
+    </div>`
+    })
+}
+getDataFromFirestore()
+
+
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    card.innerHTML = ''
+    try {
+        const docRef = await addDoc(collection(db, "posts"), {
+            title: title.value,
+            description: description.value,
+            uid: auth.currentUser.uid
+        });
+        console.log("Document written with ID: ", docRef.id);
+        getDataFromFirestore()
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
 })
