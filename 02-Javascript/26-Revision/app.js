dfee3a3d62d2c5fb2c75e6a5d1b3591798150d47
Muhammad Abdullah <101110601+mabdullahjs@users.noreply.github.com>
2023-12-08 12:47:44 +0500

    revision

---
+++ 02-Javascript/26-Revision/app.js
@@ -0,0 +1,150 @@
+// console.log('hello world');
+// console.log(abdullah);
+
+// function hello() {
+//     let fruits = ['orange', 'mango', 'banana', 'apple'];
+//     fruits.splice(1, 1);
+//     hello2(fruits)
+//     console.log(fruits);
+
+// }
+
+// function hello2(fruits) {
+//     fruits.splice(2, 1);
+// }
+
+// hello()
+
+
+
+// ['orange', 'mango', 'banana', 'apple'];
+// ['orange, 'banana', 'apple'];
+// ['orange' , 'apple']
+// ['orange']
+// ['banana']
+// ['mango']
+// error
+// undefined
+
+
+
+
+
+
+
+
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+// var str1 = 'hello';
+// let str2 = 'hello2';
+// const str3 = 'hello3';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(true){
+//     var str = 'abc';
+//     let str2 = 'cde';
+// }
+
+// console.log(str);
+// console.log(str2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const num1 = 10;
+// console.log(num1 ** 3);
+
+function hello(){
+    let h1 = 'abc';
+    // console.log(h2);
+    function hello2(){
+        let h2 = 'cde';
+        console.log(h1);
+    }
+}
+
+
+hello()()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
